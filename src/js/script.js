9cc0013a714ba5dd3d79135cfc5288f93c623104
LarsVanderheydt <lars06@hotmail.be>
2017-03-27 19:44:07 +0200

    video added (+ skip button)

--- src/js/script.js
+++ src/js/script.js
@@ -3,14 +3,21 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './containers/App';
+const $video = document.querySelector(`video`);
 
 const init = () => {
+  const $skipButton = document.querySelector(`.video_skip`);
+  $skipButton.addEventListener(`click`, clickHandler);
 
   render(
     <App  />,
     document.querySelector(`.quiz`)
   );
+};
 
+const clickHandler = () => {
+  $video.currentTime += ($video.duration - $video.currentTime);
+  $video.pause();
 };
 
 init();
