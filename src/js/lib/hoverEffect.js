72a6d3657576b2de2e9b2a91a93a38642200a4cf
YvesMeeuws <yves-laurent.meeuws@student.howest.be>
2017-03-30 12:01:55 +0200

    background position effect - sunglasses

---
+++ src/js/lib/hoverEffect.js
@@ -0,0 +1,12 @@
+const hoverEffect = ($div, e) => {
+  const movementStrength = 25;
+  const height = movementStrength / window.innerHeight;
+  const width = movementStrength / window.innerWidth;
+  const pageX = e.clientX - window.innerWidth / 2;
+  const pageY = e.clientY - window.innerHeight / 2;
+  const newvalueX = width * pageX * - 1;
+  const newvalueY = height * pageY * - 1;
+  $div.setAttribute(`style`, `background-position: ${newvalueX  }px     ${  newvalueY}px`);
+};
+
+export default hoverEffect;
